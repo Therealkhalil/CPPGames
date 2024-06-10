50fa7266ec227217de3c5edc4a4a4217b5ed1563
Khalil Chabir <khalil.chabir96@gmail.com>
2024-06-10 06:00:43 +0100

    Refactoring

--- Axe_Game/Axe_Game.cpp
+++ Axe_Game/Axe_Game.cpp
@@ -3,8 +3,8 @@
 int main()
 {
 	//Window dimension
-	int height = 200;
-	int width = 350;
+	int height = 450;
+	int width = 800;
 
 	InitWindow(width, height, "Khalil");
 
@@ -18,8 +18,13 @@ int main()
 	// Circle dimension
 	int circle_x{ width / 2 };
 	int circle_y{ height / 2 };
-	int rad{ 25 };
+	int circle_rad{ 25 };
 
+	// Axe Cordinate
+	int axe_x{ 400 };
+	int axe_y{ 0 };
+	int axe_size_x{ 50 };
+	int axe_size_y{ 50 };
 	// Update loop
 	while (WindowShouldClose() == false)
 	{
@@ -27,14 +32,18 @@ int main()
 		ClearBackground(WHITE);
 
 		//Game logic
-		DrawCircle(circle_x, circle_y, rad, BLUE);
-		if (IsKeyDown(KEY_D)) {
+		DrawCircle(circle_x, circle_y, circle_rad, BLUE);
+		DrawRectangle(axe_x, axe_y, axe_size_x, axe_size_y, RED);
+
+		if (IsKeyDown(KEY_D) && circle_x < width) {
 			circle_x += 10;
 		}
-		if (IsKeyDown(KEY_A)) {
+		if (IsKeyDown(KEY_A) && circle_x > 0) {
 			circle_x -= 10;
 		}
-
+		// axe falling down
+		if(axe_y < height-50)
+		axe_y += 10;
 		EndDrawing();
 	}
 	return 0;
