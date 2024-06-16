685347be724fe0f4bd2251fc7bf3d965898188a7
Khalil Chabir <khalil.chabir96@gmail.com>
2024-06-16 03:36:24 +0100

    add enemy

--- Dapper_Dasher/Dapper_Dasher.cpp
+++ Dapper_Dasher/Dapper_Dasher.cpp
@@ -15,15 +15,30 @@ int main()
 	//Character (Rectangle)
 	/*const int width{ 50 };
 	const int height{ 80 };
-    int posY{ windowHeight - height };*/
+	int posY{ windowHeight - height };*/
 
 	// Physics
-    int velocity{ 0 };
+	int velocity{ 0 };
 	const int jumpVelocity{ -600 }; // jumpvel : pixel per second
 	const int gravity{ 1'000 }; // gravity : (pixel/sec) per second
-    bool isInAir{ false };
+	bool isInAir{ false };
+	//for nebula velocity
+	int velocityNebula{ -200 };
 
-	//Character (Sprite)
+	//Enemy Nebula (Sprite)
+	Texture2D nebula = LoadTexture("../Dapper_Dasher/textures/12_nebula_spritesheet.png");
+	Rectangle nebulaRect;
+	Vector2 nebulaPos;
+
+	nebulaRect.x = 0;
+	nebulaRect.y = 0;
+	nebulaRect.width = nebula.width / 8;
+	nebulaRect.height = nebula.height / 8;
+
+	nebulaPos.x = windowsWidth;
+	nebulaPos.y = windowHeight - nebulaRect.height;
+
+	//Character Scrafy (Sprite)
 	Texture2D scarfy = LoadTexture("../Dapper_Dasher/textures/scarfy.png");
 	Rectangle scarfyRect;
 	Vector2 scarfyPos;
@@ -33,12 +48,26 @@ int main()
 	scarfyRect.width = scarfy.width / 6;
 	scarfyRect.height = scarfy.height;
 
-	scarfyPos.x = windowsWidth/2 - scarfyRect.width/2;
+	scarfyPos.x = windowsWidth / 2 - scarfyRect.width / 2;
 	scarfyPos.y = windowHeight - scarfyRect.height;
 
-	//End Character Parametrs
+	//Animation Frame Setup
+	const float updateTime{ 1.0 / 12.0 };
+	const float updateTimeNebula{ 1.0 / 12.0 };
 
-	SetTargetFPS(10);
+	//for scarfy
+	int frame{};
+	float runningTime{};
+
+	//for nebula
+	int frameNebula{};
+	float runningTimeNebula{};
+
+
+
+
+
+	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
 
@@ -47,7 +76,7 @@ int main()
 
 		BeginDrawing();
 		ClearBackground(WHITE);
-		
+
 
 		// check if he is on the ground 
 		if (scarfyPos.y >= windowHeight - scarfyRect.height) {
@@ -64,14 +93,46 @@ int main()
 			velocity += jumpVelocity;
 		}
 
-		// update position
+		// Update position
 		scarfyPos.y += velocity * dT;
 
-		DrawTextureRec(scarfy,scarfyRect,scarfyPos,WHITE);
 
+		//nebula update position
+		nebulaPos.x += velocityNebula * dT;
+
+		// Animation system
+		// Scarfy
+		if (!isInAir)
+		{
+			runningTime += dT;
+			if (runningTime >= updateTime) {
+				runningTime = 0;
+				scarfyRect.x = frame * scarfyRect.width;
+				frame++;
+				if (frame > 5) {
+					frame = 0;
+				}
+			}
+		}
+
+		//Nebula
+		runningTimeNebula += dT;
+		if (runningTimeNebula >= updateTimeNebula) {
+			runningTimeNebula = 0;
+			nebulaRect.x = frameNebula * nebulaRect.width;
+			frameNebula++;
+			if (frameNebula > 5) {
+				frameNebula = 0;
+			}
+		}
+
+		//draw characters
+		DrawTextureRec(scarfy, scarfyRect, scarfyPos, WHITE);
+		DrawTextureRec(nebula, nebulaRect, nebulaPos, WHITE);
 		EndDrawing();
 	}
 	UnloadTexture(scarfy);
+	UnloadTexture(nebula);
 	CloseWindow();
 	return 0;
 }
